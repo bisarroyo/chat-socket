59e63bd39f8e109e7562bc4ee33fdd6d791c9ea9
Bismark Arroyo <bis.arroyo@gmail.com>
2023-10-12 21:05:28 -0600

    add express and socket io with inital conf

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('an user has been connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('message:' + msg)
+  })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, () => {
+  console.log(`connected on port ${port}`)
+})
