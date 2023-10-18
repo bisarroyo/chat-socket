d0e9519987c31b24624b01cca350ab233f2075da
Bismark Arroyo <bis.arroyo@gmail.com>
2023-10-17 20:42:12 -0600

    Add socket to client and server

--- server/index.js
+++ server/index.js
@@ -1,24 +1,69 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 
-io.on('connection', (socket) => {
+const db = createClient({
+  url: 'libsql://nearby-supergran-bisarroyo.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+await db.execute(`
+CREATE TABLE IF NOT EXISTS messages(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT,
+  user TEXT
+)
+`)
+
+io.on('connection', async (socket) => {
   console.log('an user has been connected')
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    console.log('message:' + msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+    console.log({ username })
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, user) VALUES (:msg, :username)',
+        args: { msg, username }
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   })
+  // console.log(socket.handshake.auth)
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+      results.rows.forEach((row) => {
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 })
 
 app.get('/', (req, res) => {
